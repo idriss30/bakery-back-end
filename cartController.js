62dd487c5fcda49971aa3a1c806ce70ad95c8e3e
idriss30 <idriss.cissoko@hotmail.com>
2022-02-21 04:55:16 -0500

    added same item quantity limit of 2

--- cartController.js
+++ cartController.js
@@ -4,14 +4,28 @@ const logger = require("./logger");
 const carts = new Map();
 
 const addItemToCart = (username, item) => {
-  removeFromInventory(item);
   const newItems = (carts.get(username) || []).concat(item);
+  if (!complyToQty(newItems)) {
+    const limitError = new Error(`can't have more than 2 of the same item `);
+    limitError.code = 400;
+    throw limitError;
+  }
+  removeFromInventory(item);
   carts.set(username, newItems);
   logger.log(`${item} was added to ${username}'s cart`);
   return newItems;
 };
 
+//addingMax items of 3
+const complyToQty = (cart) => {
+  const unitPerItem = cart.reduce((itemMap, itemName) => {
+    const quantity = (itemMap[itemName] || 0) + 1;
+    return { ...itemMap, [itemName]: quantity };
+  }, {}); // initialize with empty object
+  return Object.values(unitPerItem).every((itemQty) => itemQty < 3);
+};
 module.exports = {
   carts,
   addItemToCart,
+  complyToQty,
 };
