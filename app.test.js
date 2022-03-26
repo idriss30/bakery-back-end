6d6e72eb33f51cf6567ce7a8c9ba809ff3fec96b
idriss30 <idriss.cissoko@hotmail.com>
2022-03-26 05:29:40 -0400

    modified unavailable items test, and removed non related user test

--- app.test.js
+++ app.test.js
@@ -2,6 +2,7 @@ const { app } = require("./app");
 const { carts, addItemToCart } = require("./cartController");
 const { inventory } = require("./inventoryController");
 const request = require("supertest");
+const { response } = require("express");
 
 afterAll(() => app.close());
 afterEach(() => inventory.clear());
@@ -9,10 +10,17 @@ beforeEach(() => carts.clear());
 
 describe("checking cart features", () => {
   test("adding unavailable items", async () => {
-    await request(app)
+    carts.set("idris", []);
+    const requestResponse = await request(app)
       .post("/carts/idris/items/")
       .send({ item: "cheesecake", quantity: 2 })
-      .expect(400);
+      .expect(400)
+      .expect("Content-type", "application/json; charset=utf-8");
+
+    expect(await requestResponse.body).toEqual({
+      message: "cheesecake is not available",
+    });
+    expect(carts.get("idris")).toEqual([]);
   });
 
   test("adding available items to the cart", async () => {
@@ -25,22 +33,6 @@ describe("checking cart features", () => {
 
     expect(await addingResponse.body).toEqual(["cheesecake"]);
     expect(inventory.get("cheesecake")).toEqual(0);
-  });
-
-  test("fetch unavailable user cart", async () => {
-    await request(app).get("/carts/idris/items").expect(404);
-  });
-
-  test("fetch available user cart", async () => {
-    inventory.set("cheesecake", 2);
-
-    addItemToCart("idris", "cheesecake");
-    const getCartResponse = await request(app)
-      .get("/carts/idris/items")
-      .expect(200)
-      .expect("Content-Type", "application/json; charset=utf-8");
-    expect(await getCartResponse.body).toEqual(["cheesecake"]);
-    expect(inventory.get("cheesecake")).toEqual(1);
   });
 
   test("deleting available item in a cart", async () => {
