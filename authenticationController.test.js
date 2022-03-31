2438585d11ef0e5d595c4c189d23cc8f72020bab
idriss30 <idriss.cissoko@hotmail.com>
2022-03-31 07:39:15 -0400

    wrote integration test of middleware by mocking requests

---
+++ authenticationController.test.js
@@ -0,0 +1,96 @@
+const crypto = require("crypto");
+const {
+  hashedPassword,
+  users,
+  credentialsAreValid,
+  authenticationMiddleware,
+} = require("./authenticationController");
+
+describe("testing passwords", () => {
+  test("hashing passwords", async () => {
+    const passwordToHash = "password_test";
+    const hashForPassword = crypto.createHash("sha256");
+    hashForPassword.update(passwordToHash);
+    const expectedPassword = hashForPassword.digest("hex");
+    const actualPassword = hashedPassword("password_test");
+
+    expect(expectedPassword).toEqual(actualPassword);
+  });
+});
+
+describe("credentialAreValid", () => {
+  test("validating credentials", () => {
+    users.set("test_user", {
+      email: "test@email.org",
+      password: hashedPassword("pass123"),
+    });
+
+    const checkCredentials = credentialsAreValid("test_user", "pass123");
+    expect(checkCredentials).toBe(true);
+  });
+});
+
+describe("authentication middleware", () => {
+  //integration test
+  test("returning error when credentials are not valid", async () => {
+    const fakeAuth = Buffer.from("invalid:credentials").toString("base64");
+
+    const mockedRequest = () => {
+      const req = {
+        headers: { authorization: `Basic ${fakeAuth}` },
+      };
+      return req;
+    };
+
+    const mockedResponse = () => {
+      const res = {};
+      res.status = jest.fn().mockReturnValue(res);
+      res.json = jest.fn().mockReturnValue(res);
+      return res;
+    };
+    const req = mockedRequest(),
+      res = mockedResponse(),
+      next = jest.fn();
+
+    await authenticationMiddleware(req, res, next);
+    expect(next.mock.calls).toHaveLength(0);
+    expect(res.json).toHaveBeenCalledWith({
+      message: "provide valid credentials",
+    });
+    expect(res.status).toHaveBeenCalledWith(401);
+  });
+
+  test("providing valid credentials", async () => {
+    const unhashPassword = "pass123",
+      hashingPassword = hashedPassword(unhashPassword);
+    users.set("test_user", {
+      email: "test@email.com",
+      password: hashingPassword,
+    });
+    const realAuth = Buffer.from(`test_user:${unhashPassword}`).toString(
+      "base64"
+    );
+
+    const mockedReq = () => {
+      const req = {
+        headers: { authorization: `Basic ${realAuth}` },
+      };
+
+      return req;
+    };
+
+    const mockedRes = () => {
+      const res = {};
+      res.status = jest.fn().mockReturnValue(res);
+      res.json = jest.fn().mockReturnValue(res);
+
+      return res;
+    };
+    const next = jest.fn(),
+      req = mockedReq(),
+      res = mockedRes();
+
+    await authenticationMiddleware(req, res, next);
+    expect(next.mock.calls).toHaveLength(1);
+  });
+});
