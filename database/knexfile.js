ef356ef66e95dc08a73eacc5e89c173e9c291e4e
idriss30 <idriss.cissoko@hotmail.com>
2022-04-13 09:09:34 -0400

    installed knex and sqlite to integrate database

---
+++ database/knexfile.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: { filename: path.join(__dirname, "./dev.sqlite") },
+    useNullAsDefault: true,
+  },
+};
