dce6b7ea2ca50dcea3b88d93b240fc1aad4bd7ec
idriss30 <idriss.cissoko@hotmail.com>
2022-04-13 09:16:13 -0400

    updated all routes with database and added authentication

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { carts, addItemToCart } = require("./cartController");
-const { inventory } = require("./inventoryController");
+const { addItemToCart, getUserId } = require("./cartController");
+const { addItemToInventory } = require("./inventoryController");
 const {
-  users,
-  hashedPassword,
   authenticationMiddleware,
+  createUser,
 } = require("./authenticationController");
+const { db } = require("./database/dbConnection");
 
 const app = express();
 
@@ -18,48 +18,63 @@ app.use(async (req, res, next) => {
   next();
 });
 
-app.put("/users/:username", (req, res) => {
+app.put("/users/:username", async (req, res) => {
   const username = req.params.username;
   const { email, password } = req.body;
-  const isUserAvailable = users.has(username);
-  if (isUserAvailable) {
-    res.status(409).json({ message: `${username} already exist` }); // 409 a request conflict with the current state of the target resource. Conflicts are most likely to occur in response to a PUT request
-  }
-
-  const protectPassword = hashedPassword(password);
-  users.set(username, { email, password: protectPassword });
-
-  res.status(201).json({ message: `${username} created successfully` });
-});
-
-app.get("/carts/:username/items/", (req, res) => {
-  const cart = carts.get(req.params.username);
-  cart ? res.json(cart) : res.sendStatus(404);
-});
-
-app.post("/carts/:username/items/", (req, res) => {
-  const { username } = req.params;
-  const { item, quantity } = req.body;
-
   try {
-    for (let i = 0; i < quantity; i++) {
-      const newItems = addItemToCart(username, item);
-      res.json(newItems);
+    const userResponse = await createUser(username, password, email);
+    if (userResponse == true) {
+      res.status(201).json({ message: `${username} created successfully` });
     }
   } catch (error) {
-    res.status(error.code).json({ message: error.message });
+    res.status(409).json({ message: error.message });
   }
 });
 
-app.delete("/carts/:username/items/:item", (req, res) => {
-  const { username, item } = req.params;
-  if (!carts.has(username) || !carts.get(username).includes(item)) {
-    res.status(400).json(`${item} is not in the cart`);
+app.get("/carts/:username/items/", async (req, res) => {
+  const username = req.params.username;
+  const userId = await getUserId(username);
+  const userCart = await db.select().from("carts").where({ userId });
+  userCart.length > 0 ? res.status(200).json(userCart) : res.sendStatus(404);
+});
+
+app.post("/carts/:username/items/", async (req, res) => {
+  const username = req.params.username;
+  let { item, quantity } = req.body;
+  let items;
+  try {
+    do {
+      quantity -= 1;
+      items = await addItemToCart(username, item);
+    } while (quantity > 0);
+
+    res.status(201).json(items);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
-  const newItems = (carts.get(username) || []).filter((item) => item !== item);
-  carts.set(username, newItems);
-  inventory.set(item, inventory.get(item) ? inventory.get(item) - 1 : 0);
-  res.json(newItems);
+});
+
+app.delete("/carts/:username/items/:item", async (req, res) => {
+  let { username, item } = req.params;
+  const userId = await getUserId(username);
+  const cart = await db
+    .select()
+    .from("carts")
+    .where({ userId, itemName: item });
+
+  if (cart.length > 0) {
+    if (cart.itemQty > 1) {
+      await db("carts").decrement("itemQty").where({ userId, itemName: item });
+    } else {
+      await db("carts").del().where({ userId, itemName: item });
+    }
+
+    const items = await db("carts").select().where({ userId });
+    await addItemToInventory(item, 1);
+    res.status(400).json(items);
+  } else {
+    res.status(404).json({ message: `${item} not in cart` });
+  }
 });
 
 module.exports = {
