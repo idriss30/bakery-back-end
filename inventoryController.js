92c43fc82442f36fe16bc979ea38ebde472b83c0
idriss30 <idriss.cissoko@hotmail.com>
2022-04-13 09:13:01 -0400

    switched to database and updated the test

--- inventoryController.js
+++ inventoryController.js
@@ -1,15 +1,39 @@
-const inventory = new Map();
+const { db } = require("./database/dbConnection");
 
-const removeFromInventory = (item) => {
-  if (!inventory.has(item) || !inventory.get(item) > 0) {
-    const err = new Error(`${item} is not available`);
-    err.code = 400;
-    throw err;
+const addItemToInventory = async (productName, productQty) => {
+  const product = await db("inventory").select().where({ productName }).first();
+  if (!product) {
+    await db("inventory").insert({ productName, productQty });
+    return true;
   }
-  inventory.set(item, inventory.get(item) - 1);
+  await db("inventory")
+    .where({ productName })
+    .increment({ productQty: productQty });
+  return true;
+};
+
+const removeFromInventory = async (item) => {
+  const product = await db
+    .select()
+    .from("inventory")
+    .where({ productName: item })
+    .first();
+
+  if (!product) {
+    throw new Error(`${item} is not available`);
+  }
+
+  if (product.productQty === 1) {
+    await db("inventory").where("productName", item).del();
+    return `${product.productName} has been deleted`;
+  }
+
+  return await db("inventory")
+    .where({ productName: item })
+    .decrement({ productQty: 1 });
 };
 
 module.exports = {
-  inventory,
+  addItemToInventory,
   removeFromInventory,
 };
--- inventoryController.test.js
+++ inventoryController.test.js
@@ -1,18 +1,69 @@
-const { inventory, removeFromInventory } = require("./inventoryController");
+const { db } = require("./database/dbConnection");
+const {
+  addItemToInventory,
+  removeFromInventory,
+} = require("./inventoryController");
 
-afterEach(() => inventory.clear());
+afterAll(() => db.destroy());
 
-describe("checking inventory features", () => {
-  test("remove available  ", () => {
-    inventory.set("cheesecake", 1);
-    removeFromInventory("cheesecake");
+describe("checking removeFromInventory", () => {
+  beforeEach(() => db("inventory").truncate());
+  test("remove available item  ", async () => {
+    await addItemToInventory("cheesecake", 2);
+    await removeFromInventory("cheesecake");
 
-    expect(inventory.get("cheesecake")).toEqual(0);
+    const fetchCheeseInventory = await db("inventory")
+      .select()
+      .where({ productName: "cheesecake" })
+      .first();
+
+    expect(fetchCheeseInventory.productQty).toEqual(1);
   });
 
   test("remove unavailable", async () => {
-    expect(() => removeFromInventory("cheesecake")).toThrow(
-      "cheesecake is not available"
-    );
+    try {
+      await removeFromInventory("cheesecake");
+    } catch (error) {
+      const err = new Error("cheesecake is not available");
+      expect(err).toEqual(error);
+    }
+    expect.assertions(1);
+  });
+
+  test("remove when quantity = 1", async () => {
+    const addCheese = await addItemToInventory("cheesecake", 1);
+    const inventoryDeleteResponse = await removeFromInventory("cheesecake");
+    expect(addCheese).toEqual(true);
+
+    expect(inventoryDeleteResponse).toEqual("cheesecake has been deleted");
+  });
+});
+describe("adding items to inventory", () => {
+  beforeEach(() => db("inventory").truncate());
+  test("check insert part of function", async () => {
+    await addItemToInventory("cheesecake", 2);
+    const checkCheeseInventory = await db("inventory")
+      .select()
+      .where({ productName: "cheesecake" });
+    expect(checkCheeseInventory).toEqual([
+      {
+        productName: "cheesecake",
+        productQty: 2,
+      },
+    ]);
+  });
+
+  test("check increment part of the function", async () => {
+    await addItemToInventory("danish", 1);
+    await addItemToInventory("danish", 2);
+    const fetchDanish = await db("inventory")
+      .select()
+      .where({ productName: "danish" });
+    expect(fetchDanish).toEqual([
+      {
+        productName: "danish",
+        productQty: 3,
+      },
+    ]);
   });
 });
