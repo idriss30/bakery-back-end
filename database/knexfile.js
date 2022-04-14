85951828b793f8f1d42748b84ab12481495d4404
idriss30 <idriss.cissoko@hotmail.com>
2022-04-14 17:56:50 -0400

    added migrations folder

--- database/knexfile.js
+++ database/knexfile.js
@@ -5,5 +5,14 @@ module.exports = {
     client: "sqlite3",
     connection: { filename: path.join(__dirname, "./dev.sqlite") },
     useNullAsDefault: true,
+    migrations: { directory: "./database/migrations/" },
+  },
+  test: {
+    client: "sqlite3",
+    connection: { filename: path.join(__dirname, "./test.sqlite") },
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.join(__dirname, "/migrations"),
+    },
   },
 };
