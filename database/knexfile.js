b2cd01b0e8079ab5bd087b1872112715e9151e67
idriss30 <idriss.cissoko@hotmail.com>
2022-04-15 16:11:45 -0400

    remove migration directory

--- database/knexfile.js
+++ database/knexfile.js
@@ -5,14 +5,11 @@ module.exports = {
     client: "sqlite3",
     connection: { filename: path.join(__dirname, "./dev.sqlite") },
     useNullAsDefault: true,
-    migrations: { directory: "./database/migrations/" },
+    migrations: { directory: path.join(__dirname, "/migrations") },
   },
   test: {
     client: "sqlite3",
     connection: { filename: path.join(__dirname, "./test.sqlite") },
     useNullAsDefault: true,
-    migrations: {
-      directory: path.join(__dirname, "/migrations"),
-    },
   },
 };
