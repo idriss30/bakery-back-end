b629c4b63514a9782fb13d01ee58406260ea5f4a
idriss30 <idriss.cissoko@hotmail.com>
2022-04-15 16:12:15 -0400

    tables update

--- database/migrateDatabase.js
+++ database/migrateDatabase.js
@@ -1,9 +1,15 @@
+const { path } = require("express/lib/application");
+
 require("dotenv").config();
 const environmentName = process.env.NODE_ENV || "test";
 const environtmentConfig = require("./knexfile")[environmentName];
 const db = require("knex")(environtmentConfig);
 
+const migrationConfig = {
+  directory: "./database/migrations",
+};
+
 module.exports = async () => {
-  await db.migrate.latest();
+  await db.migrate.latest(migrationConfig);
   await db.destroy();
 };
