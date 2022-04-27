58b6713d53ea6b06134a1751c6acdda494d0442d
idriss30 <idriss.cissoko@hotmail.com>
2022-04-27 09:54:09 -0400

    writing monitorSales test to see it fail (time constraint)

--- cartController.test.js
+++ cartController.test.js
@@ -1,4 +1,4 @@
-const { addItemToCart } = require("./cartController");
+const { addItemToCart, monitorStaleItems } = require("./cartController");
 const { addItemToInventory } = require("./inventoryController");
 const fs = require("fs");
 const os = require("os");
@@ -27,12 +27,13 @@ describe("testing carts function", () => {
   test("adding available items to the cart", async () => {
     await addItemToInventory("cheesecake", 3);
     const cart = await addItemToCart(globalUser.username, "cheesecake");
-    const cartMockup = [{ userId: 1, itemName: "cheesecake", itemQty: 1 }];
+
     const cheesecakeinv = await db("inventory")
       .select("productQty")
       .where({ productName: "cheesecake" })
       .first();
-    expect(cart).toEqual(cartMockup);
+    const cartRes = await db("carts").select().where({ userId: globalUser.id });
+    expect(cart).toEqual(cartRes);
     expect(cheesecakeinv.productQty).toEqual(2);
   });
 
@@ -53,5 +54,38 @@ describe("testing carts function", () => {
 
     const fileRes = fs.readFileSync(`${finalPath}`, "utf-8");
     expect(fileRes).toContain(`croissant added to test_user cart\n`);
+  });
+});
+
+describe("times functionnalities", () => {
+  const waitInMs = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  const hoursInMs = (n) => 3600 * 1000 * n;
+
+  let timer;
+  afterEach(() => {
+    if (timer) clearTimeout(timer);
+  });
+
+  test("removing stales items", async () => {
+    await addItemToInventory("cheesecake", 2);
+    await addItemToCart(globalUser.username, "cheesecake");
+    await waitInMs(hoursInMs(4));
+    timer = monitorStaleItems();
+    await waitInMs(hoursInMs(2));
+
+    const cartContent = await db("carts")
+      .select("")
+      .where({ userId: globalUser.id });
+    expect(cartContent).toEqual([]);
+
+    const cheeseInventory = await db("inventory")
+      .select("")
+      .where("productName", "cheesecake");
+    expect(cheeseInventory).toEqual([
+      { productName: "cheesecake", productQty: 2 },
+    ]);
   });
 });
